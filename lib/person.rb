c20d96de92684b1c5c8527993705b42e227319e0
Elaine Anderson <eparie@bu.edu>
2018-05-14 19:34:50 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,29 @@
+
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+    def name= (name)
+      @name = name
+    end 
+
+    def name
+      @name
+    end
+end
+
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
